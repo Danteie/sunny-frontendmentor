c7bca1164d39485c6014759ffc72ef30ae0c7113
unknown <aleksandar.djuratovic2@gmail.com>
2022-05-23 14:04:30 +0200

    Updates

---
+++ precache-manifest.ae5f38744678fe2c545a235dae16767f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "38528ddfa4adf55fb1dc",
+    "url": "/sunny-frontendmentor/static/css/main.a765a6b1.chunk.css"
+  },
+  {
+    "revision": "38528ddfa4adf55fb1dc",
+    "url": "/sunny-frontendmentor/static/js/main.38528ddf.chunk.js"
+  },
+  {
+    "revision": "4f473d823a9b02f8d234",
+    "url": "/sunny-frontendmentor/static/js/1.4f473d82.chunk.js"
+  },
+  {
+    "revision": "221189d6237f59ebe908",
+    "url": "/sunny-frontendmentor/static/js/2.221189d6.chunk.js"
+  },
+  {
+    "revision": "0b762f64841c750f9b6e",
+    "url": "/sunny-frontendmentor/static/js/runtime~main.0b762f64.js"
+  },
+  {
+    "revision": "156bc3e9950409b4acc42ca5be1d1422",
+    "url": "/sunny-frontendmentor/static/media/icon-arrow-down.156bc3e9.svg"
+  },
+  {
+    "revision": "73526a5a2ebe420a5a64f1f15a3398f7",
+    "url": "/sunny-frontendmentor/static/media/image-gallery-milkbottles.73526a5a.jpg"
+  },
+  {
+    "revision": "9492b08e6a6142d5192edd677ccaa06c",
+    "url": "/sunny-frontendmentor/static/media/image-gallery-orange.9492b08e.jpg"
+  },
+  {
+    "revision": "fc9801729a5b3437ab4687cf1bfcf093",
+    "url": "/sunny-frontendmentor/static/media/image-gallery-cone.fc980172.jpg"
+  },
+  {
+    "revision": "c76bcc858777eb21ad92f52c49f7681e",
+    "url": "/sunny-frontendmentor/static/media/image-gallery-sugarcubes.c76bcc85.jpg"
+  },
+  {
+    "revision": "2c0eec7b6f62cd71b9f20530a2819aa9",
+    "url": "/sunny-frontendmentor/static/media/fb.2c0eec7b.webp"
+  },
+  {
+    "revision": "74f57d1aaea17d1aec3477dd6ef559a8",
+    "url": "/sunny-frontendmentor/static/media/insta.74f57d1a.webp"
+  },
+  {
+    "revision": "32d14f43b7b8570b23cdc8c86f2c28f8",
+    "url": "/sunny-frontendmentor/static/media/twiter.32d14f43.webp"
+  },
+  {
+    "revision": "67ca75f3c423db065391ec1fb4919062",
+    "url": "/sunny-frontendmentor/static/media/pint.67ca75f3.webp"
+  },
+  {
+    "revision": "ff7d0ec73d3eeb8374d90ddfa184ae5d",
+    "url": "/sunny-frontendmentor/static/media/image-header.ff7d0ec7.jpg"
+  },
+  {
+    "revision": "ed3c6053572c6ebe05f14c342af4c37a",
+    "url": "/sunny-frontendmentor/static/media/image-photography.ed3c6053.jpg"
+  },
+  {
+    "revision": "e5f5a3b741ae44045f1f30234cc0ea8c",
+    "url": "/sunny-frontendmentor/static/media/image-graphic-design.e5f5a3b7.jpg"
+  },
+  {
+    "revision": "a7b49d250394633fdc8a980f359a18c3",
+    "url": "/sunny-frontendmentor/index.html"
+  }
+];
